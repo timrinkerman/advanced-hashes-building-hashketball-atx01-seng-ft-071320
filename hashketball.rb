68ba63460fd8ffa43fdb0e22ab930b498b156f09
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-15 16:42:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,9 +3,9 @@ def game_hash
     home: {
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
-      players: [
+      players: 
         {
-          player_name: "Alan Anderson",
+        "Alan Anderson" => {
           number: 0,
           shoe: 16,
           points: 22,
@@ -14,8 +14,8 @@ def game_hash
           steals: 3,
           blocks: 1,
           slam_dunks: 1
-        }, {
-          player_name: "Reggie Evans",
+        }, 
+        "Reggie Evans" => {
           number: 30,
           shoe: 14,
           points: 12,
